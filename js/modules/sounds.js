be58f7148f1fd38e32eae8f15ae84de5ca782ddc
Evandro Damaso <68165752+dam450@users.noreply.github.com>
2022-07-21 17:16:47 -0300

    fix: sounds path

--- js/modules/sounds.js
+++ js/modules/sounds.js
@@ -1,20 +1,20 @@
 export default function () {
   
-  const sndClick = new Audio('../../sounds/click.mp3')
-  const sndToggle = new Audio('../../sounds/toggle.mp3')
-  const sndCuckoo = new Audio('../../sounds/cuckoo.mp3')
+  const sndClick = new Audio('https://dam450.dev/rseat-focustimer2/sounds/click.mp3')
+  const sndToggle = new Audio('https://dam450.dev/rseat-focustimer2/sounds/toggle.mp3')
+  const sndCuckoo = new Audio('https://dam450.dev/rseat-focustimer2/sounds/cuckoo.mp3')
 
 
-  const sndCrowd = new Audio('../../sounds/cafeteria.mp3')
+  const sndCrowd = new Audio('https://dam450.dev/rseat-focustimer2/sounds/cafeteria.mp3')
   sndCrowd.loop = true
 
-  const sndRain = new Audio('../../sounds/chuva.mp3')
+  const sndRain = new Audio('https://dam450.dev/rseat-focustimer2/sounds/chuva.mp3')
   sndRain.loop = true
 
-  const sndFire = new Audio('../../sounds/lareira.mp3')
+  const sndFire = new Audio('https://dam450.dev/rseat-focustimer2/sounds/lareira.mp3')
   sndFire.loop = true
 
-  const sndForest = new Audio('../../sounds/floresta.mp3')
+  const sndForest = new Audio('https://dam450.dev/rseat-focustimer2/sounds/floresta.mp3')
   sndForest.loop = true
 
   let playingAudio = new Audio()
