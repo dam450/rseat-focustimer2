98d5a7e2850b8432b798c83f66e866b762e98aa9
Dam450 <68165752+dam450@users.noreply.github.com>
2022-07-21 15:13:35 -0300

    fix: countdown function

--- js/modules/timer.js
+++ js/modules/timer.js
@@ -1,22 +1,26 @@
 import display from "./display.js"
+import sounds from "./sounds.js"
 
-let loop = 0
+
 let timeoutID = null
 let totalMinutes = 0
-let seconds = 10
+let totalSeconds = 0
 
 function countdown () {
-  let minutesCounter = display.getMinutes
-  let secondsCounter = display.getSeconds
+
+  let minutesCounter = display.getMinutes()
+  let secondsCounter = display.getSeconds()
 
   if(minutesCounter===0 && secondsCounter===0){
-    clearTimeout(timeoutID)
+    sounds().alarm()
+    reset()
     return
   }
 
-  console.log('countdown aqui ' + ++loop)
+  console.log(`countdown: ${minutesCounter}:${secondsCounter}`)
 
   timeoutID = setTimeout( () => {
+
 
     if(secondsCounter===0 && minutesCounter>0){
       minutesCounter--
@@ -30,13 +34,24 @@ function countdown () {
     countdown()
     
   }, 1000)
+}
 
+function stop() {
   clearTimeout(timeoutID)
-
 }
 
-function set (valor) {
-  seconds = valor
+function reset() {
+  clearTimeout(timeoutID)
+  display.update(totalMinutes, totalSeconds)
 }
 
-export {countdown}
+function setMinutes (minutes) {
+  totalMinutes = minutes
+}
+
+function update (minutes, seconds) {
+  totalMinutes = minutes
+  totalSeconds = seconds
+}
+
+export default { countdown, stop, update, reset, setMinutes }
